396990dbf857328183019d9fe7dc7f1e9dc5a221
r34n1m4 <vadimzuravskis@gmail.com>
2022-12-29 10:31:09 +0200

    ArrayList refactor

--- src/ArrayList/ArrayList.java
+++ src/ArrayList/ArrayList.java
@@ -3,41 +3,43 @@ package ArrayList;
 import java.util.Arrays;
 
 public class ArrayList {
-
+    //array storage
     private Object[] arrayStore;
-    private int actSize = 0;
+    //array actual size
+    private int actualSize = 0;
 
     public ArrayList(){
         arrayStore = new Object[10];
     }
     //get()
     public Object get(int index){
-        if(index < actSize){
+        if(index < actualSize){
             return arrayStore[index];
         } else {
             throw new ArrayIndexOutOfBoundsException();
         }
     }
     //add()
-    public void add(Object obj){
-        if(arrayStore.length-actSize <= 5){
+    public void add(Object object){
+        //perform check for increasing array size
+        if(arrayStore.length - actualSize <= 5){
             increaseListSize();
         }
-        arrayStore[actSize++] = obj;
+        arrayStore[actualSize++] = object;
     }
     //remove()
     public Object remove(int index){
-        if(index < actSize){
-            Object obj = arrayStore[index];
+        if(index < actualSize){
+            Object object = arrayStore[index];
             arrayStore[index] = null;
-            int tmp = index;
-            while(tmp < actSize){
-                arrayStore[tmp] = arrayStore[tmp+1];
-                arrayStore[tmp+1] = null;
-                tmp++;
+            int temp = index;
+            while(temp < actualSize){
+                arrayStore[temp] = arrayStore[temp + 1];
+                arrayStore[temp + 1] = null;
+                temp++;
             }
-            actSize--;
-            return obj;
+            actualSize--;
+            return object;
         } else {
             throw new ArrayIndexOutOfBoundsException();
         }
@@ -45,11 +47,11 @@ public class ArrayList {
     }
     //size()
     public int size(){
-        return actSize;
+        return actualSize;
     }
-
+    //increasing array size by 5 to not run out of insertions
     private void increaseListSize(){
-        arrayStore = Arrays.copyOf(arrayStore, arrayStore.length*2);
+        arrayStore = Arrays.copyOf(arrayStore, arrayStore.length + 5);
         System.out.println("\nNew length: "+ arrayStore.length);
     }
 }
--- src/ArrayList/ArrayListTest.java
+++ src/ArrayList/ArrayListTest.java
@@ -1,23 +1,26 @@
 package ArrayList;
 
 public class ArrayListTest {
-    public static void main(String a[]) {
+    public static void main(String[] args) {
         ArrayList arrayList = new ArrayList();
-        arrayList.add(2);
-        arrayList.add(5);
         arrayList.add(1);
-        arrayList.add(23);
-        arrayList.add(14);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(25);
+        arrayList.add(15);
 
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.print(arrayList.get(i) + " ");
         }
-        arrayList.add(29);
+        arrayList.add(30);
         System.out.println("Element at Index 5: " + arrayList.get(5));
         System.out.println("List size: " + arrayList.size());
         System.out.println("Removing element at index 2: " + arrayList.remove(2));
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.print(arrayList.get(i) + " ");
         }
+        //Exception check for ArrayIndexOutOfBoundsException
+        System.out.println("\nException check for ArrayIndexOutOfBoundsException");
+        arrayList.get(100);
     }
 }
