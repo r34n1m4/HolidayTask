09a2fcdbf9d28f9daa95af2bb88e22e1bacdfcbb
r34n1m4 <vadimzuravskis@gmail.com>
2023-01-02 11:32:42 +0200

    BinarySearchTree Impl

--- src/BinarySearchTree/BinarySearchTreeTest.java
+++ src/BinarySearchTree/BinarySearchTreeTest.java
@@ -5,25 +5,25 @@ public class BinarySearchTreeTest {
         //create a BST object
         BinarySearchTree bst = new BinarySearchTree();
         /* BST tree example
-              45
+              50
            /     \
-          10      90
+          25      90
          /  \    /
-        7   12  50   */
+        10   15  80   */
         //insert data into BST
-        bst.insert(45);
-        bst.insert(10);
-        bst.insert(7);
-        bst.insert(12);
-        bst.insert(90);
         bst.insert(50);
+        bst.insert(25);
+        bst.insert(10);
+        bst.insert(15);
+        bst.insert(90);
+        bst.insert(80);
         //print the BST
         System.out.println("The BST Created with input data (Left-root-right):");
         bst.inorder();
 
         //delete leaf node
-        System.out.println("\nThe BST after Delete 12 (leaf node):");
-        bst.deleteKey(12);
+        System.out.println("\nThe BST after Delete 15 (leaf node):");
+        bst.deleteKey(15);
         bst.inorder();
         //delete the node with one child
         System.out.println("\nThe BST after Delete 90 (node with 1 child):");
@@ -35,10 +35,10 @@ public class BinarySearchTreeTest {
         bst.deleteKey(45);
         bst.inorder();
         //search a key in the BST
-        boolean returnValue = bst.search (50);
-        System.out.println("\nKey 50 found in BST:" + returnValue );
-        returnValue = bst.search (12);
-        System.out.println("\nKey 12 found in BST:" + returnValue );
+        boolean returnValue = bst.search (80);
+        System.out.println("\nKey 80 found in BST:" + returnValue );
+        returnValue = bst.search (15);
+        System.out.println("\nKey 15 found in BST:" + returnValue );
     }
 }
 
